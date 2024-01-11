270239b11f5cb3214d66cd4d7a32bafefee1942b
gbp12 <gonzabonilla4896@outlook.es>
2024-01-11 13:34:32 +0000

    interactions added

--- clases/piromano.php
+++ clases/piromano.php
@@ -1,23 +1,25 @@
 <?php
+
 require_once 'persona.php';
+
 class Piromano extends Persona
 {
     const COSAS_PARA_QUEMAR = ["arbol", "coche", "edificio"];
 
-    public function __construct($id, $nombre, $correo)
+    public function __construct($id, $nombre, $correo, $edad)
     {
-        parent::__construct($id, $nombre, $correo);
+        parent::__construct($id, $nombre, $correo, $edad);
     }
 
     public function crearIncendio()
     {
         $elemento = self::seleccionarElementoAleatorio();
-        echo "Soy un piromano y voy a quemar este " . $elemento;
+        return "Soy un piromano y voy a quemar este " . $elemento . "\n";
     }
+
     private static function seleccionarElementoAleatorio()
     {
         $claveAleatoria = array_rand(self::COSAS_PARA_QUEMAR);
-
         return self::COSAS_PARA_QUEMAR[$claveAleatoria];
     }
 }
